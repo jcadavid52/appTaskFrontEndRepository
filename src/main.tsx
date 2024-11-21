fa2cfdabbfdb79226c9fc8370b6baf7ea2d88133
jcadavid <cadavidcamilo360@gmail.com>
2024-11-20 19:48:23 -0500

    modifico main

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { TaskContextProvider } from "./Context/TaskContext.tsx";
 import { TaskContextProvider2 } from "./Modules/TaskModule/Context/ContextTask.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <TaskContextProvider>
+    
       <TaskContextProvider2>
       <App/>
       </TaskContextProvider2>
-    </TaskContextProvider>
+  
   </StrictMode>
 );
