6c7fa0b71779c96ff2fa2f1d141cda8f042b44d5
jcadavid <cadavidcamilo360@gmail.com>
2024-11-20 21:52:29 -0500

    agrego context

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-// import { TaskContextProvider2 } from "./Modules/TaskModule/Context/ContextTask.tsx";
+import { TaskContextProvider2 } from "./Modules/TaskModule/Context/ContextTask.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     
-      {/* <TaskContextProvider2> */}
+      <TaskContextProvider2>
       <App/>
-      {/* </TaskContextProvider2> */}
+      </TaskContextProvider2>
   
   </StrictMode>
 );
